65d9e529cfcd6dc863b9563cfc77326ea5df3982
unknown <awsdev747@gmail.com>
2022-03-02 10:34:44 +0530

    hibernate association mapping

---
+++ Day18/hibernate/oneTomany1/src/com/demo/main/MainClass.java
@@ -0,0 +1,17 @@
+package com.demo.main;
+
+import com.demo.logic.Service;
+
+public class MainClass {
+
+	public static void main(String arg[]) {
+
+		Service service = new Service();
+		
+		service.MutipleQestionAnwer();
+		
+		System.out.println("done...");
+
+	}
+
+}
